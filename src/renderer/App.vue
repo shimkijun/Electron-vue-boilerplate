56dfaedb5fea6f4d886ee603be2f63119590340e
shimkijun <shimkijun@gmail.com>
2020-03-28 12:51:58 +0900

    error:node버전 12이상일 때 생기는 오류

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'boilerplate'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
